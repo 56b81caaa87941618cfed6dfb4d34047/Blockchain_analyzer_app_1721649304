78bf52957f0339a7111ea8450860b3bb5578658f
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-07-22 17:26:06 +0530

    Added the component page_details_cards_component_1721649366

---
+++ components/page_details_cards_component_1721649366.js
@@ -0,0 +1,31 @@
+/* Summary: Contains a section with three cards with black background and white text.
+  The three cards are side by side and each contains a detail name in bold above a detail value.
+*/
+Vue.component("page_details_cards_component_1721649366", {
+    template: `
+    <section id="page-details-cards-component" class="py-8 bg-white">
+        <h2 id="details-title" class="text-lg font-semibold mb-5 pl-4">Details</h2>
+        <ul id="details-list" class="grid gap-4 min-[480px]:grid-cols-3 text-sm">
+            <li id="project-length" class="px-5 py-4 rounded-lg bg-gradient-to-tr from-slate-950 to-slate-800 dark:from-slate-800/80 dark:to-slate-900">
+                <div class="font-medium text-slate-200 text-slate-400">Real-Time Blockchain Monitoring</div>
+                <div class="text-slate-400">Stay up-to-date with the latest blockchain transactions and activity with our real-time monitoring system.</div>
+            </li>
+            <li id="start-date" class="px-5 py-4 rounded-lg bg-gradient-to-tr from-slate-950 to-slate-800 dark:from-slate-800/80 dark:to-slate-900">
+                <div class="font-medium" id="start-date-des-1 text-slate-200 text-slate-400">Advanced Analytics Capabilities</div>
+                <time class="text-slate-400" id="start-time-des-1">Gain valuable insights into blockchain data with our powerful analytics tools and customizable reporting.</time>
+            </li>
+            <li id="end-date" class="px-5 py-4 rounded-lg bg-gradient-to-tr from-slate-950 to-slate-800 dark:from-slate-800/80 dark:to-slate-900">
+                <div class="font-medium" id="end-date-des-1 text-slate-200 text-slate-400">Secure and Decentralized</div>
+                <time class="text-slate-400" id="end-time-des-1">Our decentralized platform ensures your data remains secure and immutable on the blockchain.</time>
+            </li>
+        </ul>
+    </section>
+    `,
+    data() {
+        return {
+            expanded: false,
+            tab: null,
+        };
+    },
+});
+
